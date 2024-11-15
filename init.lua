8778fcee2e63a774db7a33b8cef5d35398ec6268
brdn <=>
2024-11-15 18:25:16 -0500

    adjusted servers

--- init.lua
+++ init.lua
@@ -533,8 +533,8 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+        clangd = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -545,6 +545,19 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        --
+        jsonls = {
+          settings = {
+            json = {
+              schemes = {
+                {
+                  fileMatch = { '*.project.json' },
+                  url = 'https://raw.githubusercontent.com/rojo-rbx/vscode-rojo/master/schemas/project.template.schema.json',
+                },
+              },
+            },
+          },
+        },
 
         lua_ls = {
           -- cmd = {...},
